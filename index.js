c469e7f141ef79bc2ad0f75aca4c0a515d08c717
Karol Kijowski <kkijowski11@gmail.com>
2022-03-18 15:26:07 +0100

    views and seed data

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Campground = require('./models/campground')
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/yelp-camp');
+}
+
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
@@ -8,6 +19,12 @@ app.set('views', path.join(__dirname, 'views'))
 app.get('/', (req, res) => {
     res.render('home')
 })
+
+app.get('/campground', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds});
+})
+
 
 
 app.listen(3002, () => {
