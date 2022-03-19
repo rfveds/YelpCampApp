02815b36548429c01c8363450f2c40c8aa8936d1
Karol Kijowski <kkijowski11@gmail.com>
2022-03-19 10:39:52 +0100

    method-override, add new campground functionality

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const methodOverride = require('method-override')
 const path = require('path');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
@@ -14,23 +15,41 @@ async function main() {
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'));
+
+
+//homepage
 app.get('/', (req, res) => {
     res.render('home')
-})
+});
 
-app.get('/campground', async (req, res) => {
+//list campgrounds
+app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds});
-})
+});
 
+//create new campground
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+//show campground
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/show', {campground})
-})
+    res.render('campgrounds/show', {campground});
+});
 
 
 app.listen(3002, () => {
-    console.log('Serving on port 3002')
+    console.log('Serving on port 3002');
 });
