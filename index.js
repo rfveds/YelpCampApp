c49aef3024048431990aedacc1c034b897fb634c
Karol Kijowski <kkijowski11@gmail.com>
2022-03-19 12:19:19 +0100

    ejs mate package

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const methodOverride = require('method-override')
 const path = require('path');
 const mongoose = require('mongoose');
-const Campground = require('./models/campground')
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
+const Campground = require('./models/campground');
 
 main().catch(err => console.log(err));
 
@@ -12,10 +13,11 @@ async function main() {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
 
