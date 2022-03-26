fcd073dd38b8a51f773663acbb24b6d1e9894238
Karol Kijowski <kkijowski11@gmail.com>
2022-03-26 11:11:41 +0100

    added author to campgrounds, showing and hiding edit/delete

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -30,6 +30,7 @@ router.get('/new', isLoggedIn, (req, res) => {
 
 router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     req.flash('success', 'Successfully made a new campground!');
     res.redirect(`/campgrounds/${campground._id}`);
@@ -37,7 +38,7 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, nex
 
 //show campground
 router.get('/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews');
+    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
     if (!campground) {
         req.flash('error', 'Cannot find that campground!');
         return res.redirect('/campgrounds');
