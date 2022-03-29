8c38d68aedaf753b1b7a3a76a46e18b45de6d86d
Karol Kijowski <kkijowski11@gmail.com>
2022-03-29 17:20:17 +0200

    fixed bootstrap

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,18 @@ const seedDB = async () => {
             author: '623ded9b8c12d8ee93029df2',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut culpa distinctio error nam non pariatur quibusdam repellendus suscipit tempore vero? Deleniti eaque eius iusto minima necessitatibus odio officia sapiente sint.',
-            price: price
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
+                    filename: 'YelpCamp/ahfnenvca4tha00h2ubt'
+                },
+                {
+                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png',
+                    filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi'
+                }
+            ]
         })
         await camp.save();
     }
