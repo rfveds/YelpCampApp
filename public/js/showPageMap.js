eb2867539244bb6561ee9aaa4a600136b54048f1
Karol Kijowski <kkijowski11@gmail.com>
2022-03-30 18:24:00 +0200

    finished map

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -1,10 +1,14 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
-    center: campground.geometry.coordinates, // starting position [lng, lat]
-    zoom: 10 // starting zoom
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center:  campground.geometry.coordinates, // starting position [lng, lat]
+    zoom: 9 // starting zoom
 });
+
+
+map.addControl(new mapboxgl.NavigationControl());
+
 
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
