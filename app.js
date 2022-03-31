8ab737b577fbf31d3d8c2731fb41defc48c48799
Karol Kijowski <kkijowski11@gmail.com>
2022-03-31 23:08:30 +0200

    add start script

--- app.js
+++ app.js
@@ -114,6 +114,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 })
 
-app.listen(3002, () => {
-    console.log('Serving on port 3002');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`);
 });
